61088bf8966a0e39050145a32ea168e310695926
thecoderzone <sylversipi@yahoo.co.uk>
2023-05-13 00:41:38 +0000

    fixed

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,8 +1,11 @@
-!/usr/bin/env python3
-"""Deletion-resilient hypermedia pagination
+#!/usr/bin/env python3
 """
+Deletion-resilient hypermedia pagination
+"""
+
 import csv
-from typing import Dict, List
+import math
+from typing import List, Dict
 
 
 class Server:
@@ -11,8 +14,6 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
-        """Initializes a new Server instance.
-        """
         self.__dataset = None
         self.__indexed_dataset = None
 
@@ -39,27 +40,43 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-        """Retrieves info about a page from a given index and with a
-        specified size.
         """
-        data = self.indexed_dataset()
-        assert index is not None and index >= 0 and index <= max(data.keys())
-        page_data = []
-        data_count = 0
-        next_index = None
-        start = index if index else 0
-        for i, item in data.items():
-            if i >= start and data_count < page_size:
-                page_data.append(item)
-                data_count += 1
-                continue
-            if data_count == page_size:
-                next_index = i
-                break
-        page_info = {
+            Get the hyper index
+
+            Args:
+                index: Current page
+                page_size: Total size of the page
+
+            Return:
+                Hyper index
+        """
+        result_dataset = []
+        index_data = self.indexed_dataset()
+        keys_list = list(index_data.keys())
+        assert index + page_size < len(keys_list)
+        assert index < len(keys_list)
+
+        if index not in index_data:
+            start_index = keys_list[index]
+        else:
+            start_index = index
+
+        for i in range(start_index, start_index + page_size):
+            if i not in index_data:
+                result_dataset.append(index_data[keys_list[i]])
+            else:
+                result_dataset.append(index_data[i])
+
+        next_index: int = index + page_size
+
+        if index in keys_list:
+            next_index
+        else:
+            next_index = keys_list[next_index]
+
+        return {
             'index': index,
             'next_index': next_index,
-            'page_size': len(page_data),
-            'data': page_data,
+            'page_size': len(result_dataset),
+            'data': result_dataset
         }
-        return page_info
